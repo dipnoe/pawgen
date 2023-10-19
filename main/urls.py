c8d3db7c81cdec94c4b43cd01356b7a390c38716
Vagner Elena <dipnoe@gmail.com>
2023-10-19 22:30:19 +0700

    change urls

--- main/urls.py
+++ main/urls.py
@@ -6,15 +6,9 @@ from main.views import *
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('category/create/', CategoryCreateView.as_view(), name='create_category'),
-    path('category/update/<int:pk>/', CategoryUpdateView.as_view(), name='update_category'),
-    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='delete_category'),
-    path('category/list/', CategoryListView.as_view(), name='list_category'),
-    path('category/detail/<int:pk>/', CategoryDetailView.as_view(), name='detail_category'),
+    path('category/list/', CategoryListView.as_view(), name='category_list'),
 
-    path('create/', ServiceCreateView.as_view(), name='create_service'),
-    path('update/<int:pk>/', ServiceUpdateView.as_view(), name='update_service'),
-    path('delete/<int:pk>/', ServiceDeleteView.as_view(), name='delete_service'),
-    path('list/', ServiceListView.as_view(), name='list_service'),
-    path('detail/<int:pk>/', ServiceDetailView.as_view(), name='detail_service'),
+    path('service/list/<int:pk>/', ServiceListView.as_view(), name='service_category_list'),
+    path('service/list/', ServiceAllListView.as_view(), name='service_all_list'),
+    path('service/detail/<int:pk>/', ServiceDetailView.as_view(), name='service_detail'),
 ]
