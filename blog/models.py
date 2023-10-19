ccec4eb69a306caa2dae1e5483e466d0a708f2a0
Vagner Elena <dipnoe@gmail.com>
2023-10-19 22:31:19 +0700

    add blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    body = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='blog/', verbose_name='Превью', **NULLABLE)
+    create_date = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
+    views_count = models.PositiveSmallIntegerField(default=0, verbose_name='Количество просмотров', editable=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
+        ordering = ['-create_date']
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from blog.models import Blog
+
 
 # Create your views here.
+class BlogView(ListView):
+    model = Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
