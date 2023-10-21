1ba323baed836729e854c2090a3719ff73628681
Vagner Elena <dipnoe@gmail.com>
2023-10-21 17:44:15 +0700

    add tag for media path

--- blog/templatetags/tags.py
+++ blog/templatetags/tags.py
@@ -9,3 +9,9 @@ register = template.Library()
 def nice_date(date):
     if isinstance(date, datetime.datetime):
         return date.strftime("%d %b")
+
+
+@register.simple_tag()
+def mediapath(image):
+    if image:
+        return f'/media/{image}'
