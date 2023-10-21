f715bfa9c5a7b8ae054ae17dba207e3e0366dbd2
Vagner Elena <dipnoe@gmail.com>
2023-10-21 17:45:24 +0700

    add fields

--- users/forms.py
+++ users/forms.py
@@ -8,13 +8,13 @@ from users.models import User
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2')
+        fields = ('email', 'phone', 'password1', 'password2')
 
 
 class UserProfileForm(StyleFormMixin, UserChangeForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar',)
+        fields = ('email', 'first_name', 'last_name', 'phone', 'city', 'avatar',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
