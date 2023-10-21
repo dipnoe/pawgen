0495bcfada413d373ddb74f80d741b42a8b7ac07
Vagner Elena <dipnoe@gmail.com>
2023-10-21 17:43:07 +0700

    add styleForm mixin

--- main/forms.py
+++ main/forms.py
@@ -3,15 +3,20 @@ from django.forms import ModelForm
 from main.models import Category, Service
 
 
-class CategoryForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
+
+class CategoryForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
 
 
-class ServiceForm(ModelForm):
-
+class ServiceForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Service
         fields = '__all__'
