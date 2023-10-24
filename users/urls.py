f1ed4b579575467584efb436096bcb4c38217ea7
Vagner Elena <dipnoe@gmail.com>
2023-10-24 18:57:58 +0700

    add approval code

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, approve_email
 
 app_name = UsersConfig.name
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('approve_email/<str:approval_code>', approve_email, name='approve_email'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
+import uuid
 
-from config.settings import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_HOST
+from config.settings import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_HOST, APPROVE_URL
+from order.models import Order
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
@@ -17,11 +21,15 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('users:login')
 
     def form_valid(self, form):
-        print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_HOST)
+        approval_code = uuid.uuid4().hex
+        form.instance.approval_code = approval_code
+
         try:
             send_mail(
-                subject='Почта подтверждена',
-                message='Добро пожаловать на PawGene! Вы успешно зарегистрировались на нашем сайте.',
+                subject='Подтверждение почты',
+                message=f'Добро пожаловать на PawGene! Вы успешно зарегистрировались на нашем сайте.\n'
+                        f'Для подтверждения почты перейдите по ссылке\n'
+                        f'{APPROVE_URL}{form.instance.approval_code}',
                 from_email=EMAIL_HOST_USER,
                 recipient_list=[form.cleaned_data['email']]
             )
@@ -38,3 +46,19 @@ class ProfileView(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+def approve_email(request, approval_code):
+    user_for_approve = get_object_or_404(User, approval_code=approval_code)
+    user_for_approve.is_approved = True
+    user_for_approve.approval_code = None
+
+    content_type = ContentType.objects.get_for_model(Order)
+    permission = Permission.objects.get(
+        codename="set_status",
+        content_type=content_type,
+    )
+    user_for_approve.user_permissions.add(permission)
+
+    user_for_approve.save()
+    return render(request, 'users/approve_email.html')
